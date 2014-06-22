d604a8d65d98c4a3911fa7f86c51cde50ff54349
batuorhanalp <batu@orhanalp.com>
2014-06-22 19:56:24 +0300

    Log post api created

---
+++ Logger/Logger/Controllers/LogController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Logger.Models;
+
+namespace Logger.Controllers
+{
+    public class LogController : ApiController
+    {
+        // POST api/log
+        public ApiResultModel Post(List<ApiModel> logs)
+        {
+            try
+            {
+                var db = new loggerEntities();
+                foreach (var log in logs)
+                {
+                    var newLog = new log
+                    {
+                        userId = log.userId,
+                        actionDate = Convert.ToDateTime(log.dateTime),
+                        location = log.location,
+                        page = log.page,
+                        action = log.action,
+                        result = log.result
+                    };
+                    db.logs.Add(newLog);
+                    db.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                var strError = string.Format("Loglar kaydedilemedi.\nHata: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    strError = string.Format("{0}\nInnerException: {1}", strError, e.InnerException);
+                }
+                if (e.StackTrace != null)
+                {
+                    strError = string.Format("{0}\nStackTrace: {1}", strError, e.StackTrace);
+                }
+
+                return new ApiResultModel
+                {
+                    Succeed = false,
+                    Reason = strError
+                };
+            }
+            return new ApiResultModel
+            {
+                Succeed = true,
+                Reason = "
+            };
+        }
+    }
+}
---
+++ Logger/Logger/Models/ApiResultModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Logger.Models
+{
+    public class ApiResultModel
+    {
+        public bool Succeed { get; set; }
+        public string Reason { get; set; }
+    }
+
+    public class ApiModel
+    {
+        public Guid userId { get; set; }
+        public string dateTime { get; set; }
+        public string location { get; set; }
+        public string page { get; set; }
+        public string action { get; set; }
+        public string result { get; set; }
+    }
+}
